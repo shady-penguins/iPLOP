431ca69461bb16d63ed5328f2ab088f4a7c8ff81
Eric Turner <EricTurnerNV@gmail.com>
2015-08-08 10:40:50 -0700

    View created

--- iPlop/beerpong/views.py
+++ iPlop/beerpong/views.py
@@ -1,13 +1,12 @@
 from django.shortcuts import render
 from django.template import RequestContext
+import simplejson as simplejson
 from iPlop.beerpong.models import Team, Game
 
 
 def PlayGame(request, game, cups_removed):
-
-
     if game is None:
-        game = Game(team_a=Team.objects.get(pk=1), team_b = Team.objects.get(pk=2))
+        game = Game(team_a=Team.objects.get(pk=1), team_b=Team.objects.get(pk=2))
     else:
         if game.current_team == Team.objects.get(pk=1):
             game.team_a_cups_left -= cups_removed
@@ -16,6 +15,5 @@ def PlayGame(request, game, cups_removed):
         if game.current_team == 0:
             game.winner = game.current_team
             game.status = False
-        context = RequestContext(request, {'game': game})
-        return render(request, 'iPLOP.html', context)
+        return simplejson.dumps(game)
 
