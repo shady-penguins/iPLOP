769fb07afff56dd89160304909adf15e1333df12
Eric Turner <EricTurnerNV@gmail.com>
2015-08-08 16:12:23 -0700

    takes the post request, does the logic

--- iPlop/beerpong/views.py
+++ iPlop/beerpong/views.py
@@ -6,23 +6,28 @@ import simplejson as simplejson
 from django.http import JsonResponse, HttpResponse
 from iPlop.beerpong.models import Team, Game
 
+game = Game(team_a=Team.objects.get(pk=1,), team_b=Team.objects.get(pk=2,))
+
 @csrf_exempt
-def PlayGame(request, game=None, cups_removed=0):
+def PlayGame(request, cups_removed=0):
+    global game
     if request.method == "GET":
         game = Game(team_a=Team.objects.get(pk=1,), team_b=Team.objects.get(pk=2,))
         return JsonResponse(json.dumps(game, default=lambda o: o.__dict__), safe=False)
 
     if request.method == "POST":
-
+        data = request.body.decode('utf8')
+        data = json.loads(data)
+        cups_removed = data['cupsRemoved']
         if game.current_team == game.team_a:
             game.team_a_cups_left -= cups_removed
             game.current_team = game.team_pk
         else:
             game.team_b_cups_left -= cups_removed
             game.current_team = game.team_a.pk
-        if game.team_a_cups_left == 0 or game.team_b_cups_left == 0:
-            game.winner = game.current_team
-            game.status = False
+    if game.team_a_cups_left == 0 or game.team_b_cups_left == 0:
+        game.winner = game.current_team
+        game.status = False
     return JsonResponse(json.dumps(game, default=lambda o: o.__dict__), safe=False)
 
 @csrf_exempt
