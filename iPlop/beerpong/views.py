7c9d4d24dc2f67400fb6dff10cbee10bd79c193d
Eric Turner <EricTurnerNV@gmail.com>
2015-08-08 14:43:44 -0700

    unbroke the blank page

--- iPlop/beerpong/views.py
+++ iPlop/beerpong/views.py
@@ -14,11 +14,14 @@ def PlayGame(request, game=None, cups_removed=0):
     if request.method == "POST":
         if game.current_team == game.team_a:
             game.team_a_cups_left -= cups_removed
-            game.current_team = game.team_b
+            game.current_team = game.team_pk
         else:
             game.team_b_cups_left -= cups_removed
-            game.current_team = game.team_a
+            game.current_team = game.team_a.pk
         if game.team_a_cups_left == 0 or game.team_b_cups_left == 0:
             game.winner = game.current_team
             game.status = False
     return JsonResponse(json.dumps(game, default=lambda o: o.__dict__), safe=False)
+
+def Blank(request):
+    return render(request, 'blank.html')
